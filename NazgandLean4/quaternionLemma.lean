69cc4ae8e675d3445b85c3798290d5b5e5bbfa83
Mark Andrew Gerads <nazgand@gmail.com>
2023-11-19 11:36:57 -0800

    Updated to match style guidelines.

--- NazgandLean4/quaternionLemma.lean
+++ NazgandLean4/quaternionLemma.lean
@@ -12,77 +12,77 @@ lemma equalSetsSoqtstn1₀AndSoqtstn1₁ : soqtstn1₀ = soqtstn1₁ := by
   dsimp [soqtstn1₀, soqtstn1₁]
   simp [Quaternion.ext_iff]
   constructor
-  intros ha
-  use x
-  use y
-  use z
-  simp only [and_self, and_true]
-  rcases ha with ⟨hSphere3,h0x,h0y,h0z⟩
-  ring_nf at h0x
-  ring_nf at h0y
-  ring_nf at h0z
-  simp only [zero_eq_mul, mul_eq_zero, OfNat.ofNat_ne_zero, or_false] at h0x
-  simp only [zero_eq_mul, mul_eq_zero, OfNat.ofNat_ne_zero, or_false] at h0y
-  simp only [zero_eq_mul, mul_eq_zero, OfNat.ofNat_ne_zero, or_false] at h0z
-  have hr₀ : (¬ r = 0) → False := by
-    intros hrn0
-    let hrn0₂ := Iff.mpr iff_false_iff hrn0
-    simp only [hrn0₂, false_or] at h0x
-    simp only [hrn0₂, false_or] at h0y
-    simp only [hrn0₂, false_or] at h0z
-    simp_rw [h0x, h0y, h0z] at hSphere3
-    simp only [mul_zero, sub_zero] at hSphere3
-    have hrnn := mul_self_nonneg r
-    rw [←hSphere3] at hrnn
-    linarith
-  have hr : r = 0 := by_contra hr₀
-  constructor
-  exact hr
-  rw [hr] at hSphere3
-  let hSphere4 := congrArg (λ (xk : ℝ) => -xk) hSphere3
-  simp only [neg_neg, mul_zero, zero_sub, neg_sub] at hSphere4
-  rw [hSphere4]
-  ring_nf
-  intros h₀
-  ring_nf
-  rcases h₀ with ⟨rx,ry,rz,hx⟩
-  rcases hx with ⟨hx₀,hSphere⟩
-  rcases hx₀ with ⟨hr,hrx,hry,hrz⟩
-  simp_rw [hr]
-  simp only [ne_eq, zero_pow', zero_sub, zero_mul, and_self, and_true]
-  simp_rw [hrx,hry,hrz]
-  let hSphere2 := congrArg (λ (xk : ℝ) => -xk) hSphere
-  simp only [neg_add_rev] at hSphere2
-  rw [←hSphere2]
-  ring
+  · intros ha
+    use x
+    use y
+    use z
+    simp only [and_self, and_true]
+    rcases ha with ⟨hSphere3,h0x,h0y,h0z⟩
+    ring_nf at h0x
+    ring_nf at h0y
+    ring_nf at h0z
+    simp only [zero_eq_mul, mul_eq_zero, OfNat.ofNat_ne_zero, or_false] at h0x
+    simp only [zero_eq_mul, mul_eq_zero, OfNat.ofNat_ne_zero, or_false] at h0y
+    simp only [zero_eq_mul, mul_eq_zero, OfNat.ofNat_ne_zero, or_false] at h0z
+    have hr₀ : (¬ r = 0) → False := by
+      intros hrn0
+      let hrn0₂ := Iff.mpr iff_false_iff hrn0
+      simp only [hrn0₂, false_or] at h0x
+      simp only [hrn0₂, false_or] at h0y
+      simp only [hrn0₂, false_or] at h0z
+      simp_rw [h0x, h0y, h0z] at hSphere3
+      simp only [mul_zero, sub_zero] at hSphere3
+      have hrnn := mul_self_nonneg r
+      rw [←hSphere3] at hrnn
+      linarith
+    have hr : r = 0 := by_contra hr₀
+    constructor
+    · exact hr
+    · rw [hr] at hSphere3
+      let hSphere4 := congrArg (λ (xk : ℝ) => -xk) hSphere3
+      simp only [neg_neg, mul_zero, zero_sub, neg_sub] at hSphere4
+      rw [hSphere4]
+      ring_nf
+  · intros h₀
+    ring_nf
+    rcases h₀ with ⟨rx,ry,rz,hx⟩
+    rcases hx with ⟨hx₀,hSphere⟩
+    rcases hx₀ with ⟨hr,hrx,hry,hrz⟩
+    simp_rw [hr]
+    simp only [ne_eq, zero_pow', zero_sub, zero_mul, and_self, and_true]
+    simp_rw [hrx,hry,hrz]
+    let hSphere2 := congrArg (λ (xk : ℝ) => -xk) hSphere
+    simp only [neg_add_rev] at hSphere2
+    rw [←hSphere2]
+    ring
 
 lemma equalSetsSoqtstn1₁AndSoqtstn1₂ : soqtstn1₁ = soqtstn1₂ := by
   ext ⟨r, x, y, z⟩
   dsimp [soqtstn1₁, soqtstn1₂]
   simp [Quaternion.ext_iff]
   constructor
-  intros h
-  rcases h with ⟨rx,ry,rz,hx,hSphere⟩
-  rcases hx with ⟨hr0, hxrx, hyry, hzrz⟩
-  simp_rw [hr0]
-  simp only [and_true]
-  simp_rw [←hxrx, ←hyry, ←hzrz] at hSphere
-  let hNorm1 := congrArg Real.sqrt hSphere
-  simp only [Real.sqrt_one] at hNorm1
-  simp_rw [←hNorm1]
-  let hSqrtNormSquare := congrArg Real.sqrt (Quaternion.normSq_eq_norm_mul_self (@QuaternionAlgebra.mk ℝ (-1) (-1) 0 x y z))
-  simp only [norm_nonneg, Real.sqrt_mul_self] at hSqrtNormSquare
-  simp_rw [←hSqrtNormSquare, Quaternion.normSq_def']
-  ring_nf
-  intros h₀
-  rcases h₀ with ⟨hNorm1, hr0⟩
-  use x
-  use y
-  use z
-  simp only [hr0, and_self, true_and]
-  let hNormSquare1 := congrArg (λ (r₀ : ℝ)=>r₀*r₀) hNorm1
-  simp only [mul_one] at hNormSquare1
-  rw [←Quaternion.normSq_eq_norm_mul_self, hr0, Quaternion.normSq_def'] at hNormSquare1
-  rw [←hNormSquare1]
-  simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow', zero_add]
-  ring_nf
+  · intros h
+    rcases h with ⟨rx,ry,rz,hx,hSphere⟩
+    rcases hx with ⟨hr0, hxrx, hyry, hzrz⟩
+    simp_rw [hr0]
+    simp only [and_true]
+    simp_rw [←hxrx, ←hyry, ←hzrz] at hSphere
+    let hNorm1 := congrArg Real.sqrt hSphere
+    simp only [Real.sqrt_one] at hNorm1
+    simp_rw [←hNorm1]
+    let hSqrtNormSquare := congrArg Real.sqrt (Quaternion.normSq_eq_norm_mul_self (@QuaternionAlgebra.mk ℝ (-1) (-1) 0 x y z))
+    simp only [norm_nonneg, Real.sqrt_mul_self] at hSqrtNormSquare
+    simp_rw [←hSqrtNormSquare, Quaternion.normSq_def']
+    ring_nf
+  · intros h₀
+    rcases h₀ with ⟨hNorm1, hr0⟩
+    use x
+    use y
+    use z
+    simp only [hr0, and_self, true_and]
+    let hNormSquare1 := congrArg (λ (r₀ : ℝ)=>r₀*r₀) hNorm1
+    simp only [mul_one] at hNormSquare1
+    rw [←Quaternion.normSq_eq_norm_mul_self, hr0, Quaternion.normSq_def'] at hNormSquare1
+    rw [←hNormSquare1]
+    simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow', zero_add]
+    ring_nf

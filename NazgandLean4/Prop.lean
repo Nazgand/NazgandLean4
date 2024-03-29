481f688668e76a9d237f1a9904ea02edf770d989
Mark Andrew Gerads <nazgand@gmail.com>
2024-03-29 03:12:39 -0700

    lemma impliesOrImpliesGeneralized

--- NazgandLean4/Prop.lean
+++ NazgandLean4/Prop.lean
@@ -1,6 +1,6 @@
 import Mathlib
 set_option maxHeartbeats 0
-open Classical
+open Classical Finset
 
 lemma impliesOr (p q r : Prop) : (p → (q ∨ r)) ↔ ((p → q) ∨ (p → r)) := by
   tauto
@@ -23,10 +23,22 @@ lemma impliesReorder (p q r : Prop) : (p → (q → r)) ↔ (q → (p → r)) :=
 lemma impliesAndImplies (p q r : Prop) : (p → (q → r)) ↔ ((q ∧ p) → r) := by
   tauto
 
-lemma impliesOrImplies (p q r : Prop) : ((q → r) ∧ (p → r)) ↔ ((q ∨ p) → r) := by
+lemma impliesOrImplies (p₀ p₁ r : Prop) : ((p₀ → r) ∧ (p₁ → r)) ↔ ((p₀ ∨ p₁) → r) := by
   tauto
 
+lemma impliesOrImplies3 (p₀ p₁ p₂ r : Prop) : ((p₀ → r) ∧ (p₁ → r) ∧ (p₂ → r)) ↔ ((p₀ ∨ p₁ ∨ p₂) → r) := by
+  tauto
+
+lemma impliesOrImplies4 (p₀ p₁ p₂ p₃ r : Prop) : ((p₀ → r) ∧ (p₁ → r) ∧ (p₂ → r) ∧ (p₃ → r)) ↔ ((p₀ ∨ p₁ ∨ p₂ ∨ p₃) → r) := by
+  tauto
+
+lemma impliesOrImpliesGeneralized (k : ℕ+) (p : ℕ → Prop) (r : Prop) : (∀ k₀ ∈ range k, (p k₀ → r)) ↔ ((∃ k₀ ∈ range k, p k₀) → r) := by
+  aesop
+
 lemma combineCases (q r : Prop) : ((q → r) ∧ ((¬q) → r)) ↔ r := by
+  tauto
+
+lemma combineCasesGeneralized (k : ℕ+) (p : ℕ → Prop) (r : Prop) : (∃ k₀ ∈ range k, p k₀) → (r ↔ (∀ k₀ ∈ range k, (p k₀ → r))) := by
   tauto
 
 lemma squashImplies (q r : Prop) : (q → (q → r)) ↔ (q → r) := by

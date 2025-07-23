053a77e0d932972ef7cefaa98bdac25bdcbcea38
Mark Andrew Gerads <Nazgand@Gmail.Com>
2025-07-22 21:06:08 -0700

    MiscDiffEq update

--- NazgandLean4/Miscellaneous.lean
+++ NazgandLean4/Miscellaneous.lean
@@ -1,12 +1,7 @@
 import Mathlib
 set_option maxHeartbeats 0
 
-lemma MiscDiffEq (f : ℂ → ℂ) (h0 : Differentiable ℂ f) (k : ℕ) (h1 : k > 0) :
-  deriv f = (λ (z : ℂ) => (f (z / k)) ^ k) ↔
-  ∃ (c : ℂ), f = (λ (z : ℂ) => c * Complex.exp (c ^ (k-1) * z)) := by
-  constructor
-  · sorry
-  · intro h2
-    rcases h2 with ⟨c, rfl⟩
-    simp
-    sorry
+theorem MiscDiffEq (f : ℂ → ℂ) (h0 : Differentiable ℂ f) (z₀ : ℂ) (k : ℕ) (h1 : k > 0) :
+  deriv f = (λ (z : ℂ) => z₀ * (f (z / k)) ^ k) ↔
+  f = (λ (z : ℂ) => (f 0) * ((f 0) ^ (k - 1) * z₀ * z).exp) := by
+  sorry

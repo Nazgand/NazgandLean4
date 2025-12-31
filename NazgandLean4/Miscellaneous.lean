765861f6e619c0b6fe33b2cc876c0a982ff57c07
Mark Andrew Gerads <Nazgand@Gmail.Com>
2025-12-30 22:12:20 -0800

    Corrected and proved both `GeneralPigeonholePrinciple` theorems

--- NazgandLean4/Miscellaneous.lean
+++ NazgandLean4/Miscellaneous.lean
@@ -3,13 +3,44 @@ set_option maxHeartbeats 0
 
 theorem GeneralPigeonholePrinciple₀ (k : ℕ) (f : ℕ → ℝ) (slb : ℝ)
   (h0 : slb < (Finset.range (k + 1)).sum f) :
-  ∃ m ∈ (Finset.range (k + 1)), f m > slb / k :=
-  sorry
+  ∃ m ∈ (Finset.range (k + 1)), f m > slb / (k + 1) := by
+  by_contra h
+  push_neg at h
+  have h1 : (Finset.range (k + 1)).sum f ≤ ∑ i ∈ Finset.range (k + 1), slb / (k + 1) := by
+    apply Finset.sum_le_sum
+    intros i hi
+    exact h i hi
+  simp only [Finset.sum_const, Finset.card_range, nsmul_eq_mul] at h1
+  have h2 : (↑(k + 1) : ℝ) ≠ 0 := by
+    norm_cast
+  field_simp [h2] at h1
+  rw [mul_comm ((Finset.range (k + 1)).sum f)] at h1
+  have h3 : 0 < (↑(k + 1) : ℝ) := by norm_cast; linarith
+  norm_cast at h1
+  replace h1 := le_of_mul_le_mul_left h1 h3
+  linarith
 
 theorem GeneralPigeonholePrinciple₁ (k : ℕ) (f : ℕ → ℝ) (slb : ℝ)
   (h0 : slb ≤ (Finset.range (k + 1)).sum f) :
-  ∃ m ∈ (Finset.range (k + 1)), f m ≥ slb / k :=
-  sorry
+  ∃ m ∈ (Finset.range (k + 1)), f m ≥ slb / (k + 1) := by
+  by_contra h
+  push_neg at h
+  have h1 : (Finset.range (k + 1)).sum f < ∑ i ∈ Finset.range (k + 1), slb / (k + 1) := by
+    apply Finset.sum_lt_sum
+    · intros i hi
+      exact le_of_lt (h i hi)
+    · use 0
+      simp only [Finset.mem_range, Nat.zero_lt_succ, true_and]
+      exact h 0 (Finset.mem_range.mpr (Nat.zero_lt_succ k))
+  simp only [Finset.sum_const, Finset.card_range, nsmul_eq_mul] at h1
+  have h2 : (↑(k + 1) : ℝ) ≠ 0 := by
+    norm_cast
+  field_simp [h2] at h1
+  rw [mul_comm ((Finset.range (k + 1)).sum f)] at h1
+  have h3 : 0 < (↑(k + 1) : ℝ) := by norm_cast; linarith
+  norm_cast at h1
+  replace h1 := lt_of_mul_lt_mul_left h1 (le_of_lt h3)
+  linarith
 
 theorem MiscDiffEq (f : ℂ → ℂ) (h0 : Differentiable ℂ f) (z₀ : ℂ) (k : ℕ) (h1 : k > 0) :
   deriv f = (λ (z : ℂ) ↦ z₀ * (f (z / k)) ^ k) ↔

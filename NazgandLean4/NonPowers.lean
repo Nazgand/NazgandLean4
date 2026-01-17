7109cf4d4c099f7d89e4097130928570e3989972
Mark Andrew Gerads <Nazgand@Gmail.Com>
2026-01-16 17:20:41 -0800

    theorem NonPowerNatPrimeExponentsGcdEq1

--- NazgandLean4/NonPowers.lean
+++ NazgandLean4/NonPowers.lean
@@ -25,7 +25,49 @@ theorem PrimeExponentsGcdOfPower (a b : ℕ) :
 
 theorem NonPowerNatPrimeExponentsGcdEq1 :
   NonPowerNat = {k : ℕ | 1 = PrimeExponentsGcd k} := by
-  sorry
+  ext k
+  simp only [NonPowerNat, Set.mem_setOf_eq]
+  constructor
+  · intro h_np
+    by_contra h_g
+    let g := PrimeExponentsGcd k
+    have hg_ne_1 : g ≠ 1 := by exact Ne.symm (Ne.intro h_g)
+    by_cases hg0 : g = 0
+    · have : k = 0 ∨ k = 1 := by
+        simp only [g, PrimeExponentsGcd] at hg0
+        by_cases hk : k = 0; · left; exact hk
+        right
+        rw [← Nat.factorization_prod_pow_eq_self hk]
+        rw [Finset.gcd_eq_zero_iff] at hg0
+        apply Finset.prod_eq_one
+        intro p hp
+        exfalso
+        exact (Finsupp.mem_support_iff.mp hp) (hg0 p hp)
+      rcases this with rfl | rfl
+      · apply h_np; use 0, 2; simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow,
+        gt_iff_lt, Nat.one_lt_ofNat, and_self]
+      · apply h_np; use 1, 2; simp only [one_pow, gt_iff_lt, Nat.one_lt_ofNat, and_self]
+    · have hk0 : k ≠ 0 := by
+        intro hk
+        simp only [PrimeExponentsGcd, hk, Nat.factorization_zero, Finsupp.support_zero,
+          Finsupp.coe_zero, Finset.gcd_empty, not_true_eq_false, g] at hg0
+      have hg_gt_1 : g > 1 :=
+        Nat.lt_of_le_of_ne (Nat.pos_of_ne_zero hg0) hg_ne_1.symm
+      let root := k.factorization.support.prod (λ p => p ^ (k.factorization p / g))
+      have hk : k = root ^ g := by
+        rw [← Finset.prod_pow]
+        nth_rw 1 [← Nat.factorization_prod_pow_eq_self hk0]
+        refine Finset.prod_congr rfl (λ p hp => ?_)
+        rw [← Nat.pow_mul]
+        congr 1
+        have h_div : g ∣ k.factorization p := Finset.gcd_dvd hp
+        exact (Nat.div_mul_cancel h_div).symm
+      apply h_np
+      use root, g
+  · intro h_gcd ⟨a, b, hk, hb⟩
+    rw [hk, PrimeExponentsGcdOfPower] at h_gcd
+    have hb_eq_1 : b = 1 := Nat.eq_one_of_dvd_one (Dvd.intro _ h_gcd.symm)
+    linarith
 
 theorem OverUnityNatUniqueNonPowerNatBase (k : ℕ) (h : k ∈ OverUnityNat) :
   ∃! (a : ℕ), (a ∈ NonPowerNat ∧ ∃ (b : ℕ), k = a ^ b) := by
